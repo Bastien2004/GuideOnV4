79fb4993701096c5efb926c8f501f87bee50eaf5
Ruixi62 <Paul140408@outlook.fr>
2026-05-31 13:47:50 +0200

    Système birthday

--- utils/db/models/__init__.py
+++ utils/db/models/__init__.py
@@ -9,6 +9,7 @@ from utils.db.models.autorole import AutoRoleConfig
 from utils.db.models.reaction_role import ReactionRoleCouple, ReactionRoleMessage
 from utils.db.models.bug_report import BugReport
 from utils.db.models.invite import InviteConfig, InviteLink, InviteStat
+from utils.db.models.birthday import BirthdayConfig, BirthdayUser
 
 __all__ = [
     "Base",
@@ -23,4 +24,6 @@ __all__ = [
     "InviteConfig",
     "InviteLink",
     "InviteStat",
+    "BirthdayConfig",
+    "BirthdayUser",
 ]
